f02d71514414a7856b4016f0466c457069923cc9
SmokeyWen <835033838wen@gmail.com>
2019-10-02 15:57:18 -0700

    page navi for sign in/up

--- react_native/App.js
+++ react_native/App.js
@@ -1,35 +1,38 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
+import { createStackNavigator } from "react-navigation-stack";
+import { createAppContainer } from "react-navigation";
+import FirstPage from "./Components/FirstPage";
+import SignIn from "./Components/SignIn";
+import SignUp from "./Components/SignUp";
+
+
+const RootStack = createStackNavigator(
+  {
+    FirstPage: {
+      screen: FirstPage,
+    },
+    SignIn: {
+      screen: SignIn,
+    },
+    SignUp: {
+      screen: SignUp,
+    }
+  },
+  {
+    initialRouteName: "FirstPage",
+    headerMode: "none"
+  }
+);
+
+const AppContainer = createAppContainer(RootStack);
+
 
 export default class App extends React.Component {
 
-  constructor() {
-    super();
-    this.state = {
-     dbResponse: ""
-    }
-  }
-
-  retrieveData = () => {
-     fetch('http://18.236.181.191:8080')
-      .then((response) => response.json())
-      .then((responseJson) => {
-        this.setState({
-            dbResponse: JSON.stringify(responseJson)
-        })
-        console.log(responseJson);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }
-
   render(){
     return (
-      <View style={styles.container}>
-        <Button onPress={ ()=>this.retrieveData() } title=" Retrieve Data " />
-        <Text>{this.state.dbResponse}</Text>
-      </View>
+      <AppContainer />
     );
   }  
 }

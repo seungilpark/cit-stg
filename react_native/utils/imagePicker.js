72988fad421a7a3a5e56956a4c4dc02dc51e3a7e
Seungil Park <spark263@my.bcit.ca>
2019-11-11 00:37:52 -0800

    image picker / images

--- react_native/utils/imagePicker.js
+++ react_native/utils/imagePicker.js
@@ -10,3 +10,26 @@ const CLUB_IMAGES_DICT = {
     "Manchester City":"mancity",
     "Real Madrid":"realmadrid.png"
 }
+
+
+/* 
+ * @input: [] club {}
+ * @output: [] club {} + url: 
+ * url:path from component folder
+ */
+const clubImagePicker = (clubArr) => {
+    return clubArr.map(club => Object.assign({}, club, {"url": `../assets/${CLUB_IMAGES_DICT[club.club_name]}`}));
+}
+
+
+const athlImagePicker = (athlArr) => {
+    return athlArr.map((athl, index) => {
+        let gender = athl.athl_gender == "Male" ? "" : "f";
+        return Object.assign({}, athl, {"url":`../assets/athl/${gender}${index % 19 + 1}.jpg`})
+    })
+}
+
+module.exports = {
+    clubImagePicker,
+    athlImagePicker,
+}

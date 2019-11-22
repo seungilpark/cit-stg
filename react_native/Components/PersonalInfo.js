a35b8f624f96cff9444cfe3c67549c104c806d86
Ewan Watt <watt.ewan99@gmail.com>
2019-11-21 23:11:01 -0800

    Starting of front end sign up validation

--- react_native/Components/PersonalInfo.js
+++ react_native/Components/PersonalInfo.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, TextInput, ScrollView, KeyboardAvoidingView, Picker, TouchableHighlight } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, ScrollView, KeyboardAvoidingView, Picker, TouchableHighlight, Alert, alertMessage } from 'react-native';
 import { NavigationEvents } from 'react-navigation';
 // import { ScrollView } from 'react-native-gesture-handler';
 
@@ -18,91 +18,62 @@ export default class PersonalInfo extends React.Component {
         gender: 'M',
         dob: '1988-08-08',
         height: "188",
-        weight: "80"
+        weight: "80",
+        valid: false
     };
-    this.Submit = this.Submit.bind(this);
-    this.verPd = this.verPd.bind(this);
+    
     this.checkEmpty = this.checkEmpty.bind(this);
 }
 
-Submit(){
-  checkEmpty = this.checkEmpty();
-  console.log(checkEmpty);
-  if(checkEmpty != false){
-    checkPd = this.verPd();
-    console.log(checkPd);
-    if(checkPd == true && this.state.role === 'ath'){
-      fetch('http://192.168.0.106:8080/api/athletes/register', {
-        method: 'POST',
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          athl_fname: this.state.fname,
-          athl_lname: this.state.lname,
-          athl_gender: this.state.gender,
-          athl_dob: this.state.dob,
-          athl_addr: this.state.addr,
-          athl_height: this.state.height,
-          athl_weight: this.state.weight,
-          athl_email: this.state.email,
-          athl_phone: this.state.phone,
-          account: this.state.account,
-          password: this.state.password,
-          city: this.state.city,
-          country: this.state.country
-        }),
-      });
-      console.log("Athlete Created.")
-    }
-    else if(checkPd == true && this.state.role === 'mgr'){
-      fetch('http://172.20.10.3:8080/api/clubMgrs/create', {
-        method: 'POST',
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          mgr_fname: this.state.fname,
-          mgr_lname: this.state.lname,
-          // athl_gender: this.state.gender,
-          // athl_dob: this.state.dob,
-          // addr: this.state.addr,
-          // athl_height: this.state.height,
-          // athl_weight: this.state.weight,
-          mgr_email: this.state.email,
-          mgr_phone: this.state.phone,
-          mgr_account: this.state.account,
-          mgr_password: this.state.password,
-          // city: this.state.city,
-          // country: this.state.country
-        }),
-      });
-      console.log("Manager Created.")
-    }else{
-      this.setState({
-        alert: "Please confirm your password."
-      })
-    }
-  }
+
+
+
+CheckPage(){
+  const { navigation } = this.props
+  const role = navigation.getParam('role','ath')
+
+
+  this.checkEmpty(this.state.fname)
+  this.checkEmpty(this.state.lname)
+  this.checkEmpty(this.state.gender)
+  this.checkEmpty(this.state.dob)
+  this.checkEmpty(this.state.addr)
+  this.checkEmpty(this.state.height)
+  this.checkEmpty(this.state.weight)
+
+if(this.state.valid == true) {
+  this.props.navigation.navigate('LocationInfo', {
+    role: role,
+    fname: this.state.fname,
+    lname: this.state.lname,
+    gender: this.state.gender,
+    dob: this.state.dob,
+    addr: this.state.addr,
+    height: this.state.height,
+    weight: this.state.weight});
+}
 }
 
-verPd(){
-  if (this.state.password == this.state.password1){
-    return true;
-  }else{
-    return false;
-  }
+
+btnPress1(){
+  Alert.alert('You have been logged out', alertMessage, [
+
+      {text: 'OK', onPress: () => this.props.navigation.navigate('Login')},
+  ])
 }
 
-checkEmpty(){
-  var i;
-  for (i = 0; i < this.state.length; i++) {
-    if(this.state[i] == ''){
-      this.setState({alert: "Missing field(s)"});
-      return false;
-    }
+
+
+checkEmpty(entry){
+    if(this.state.entry == ''){
+      Alert.alert('Missing entry(s)', alertMessage, [
+
+        {text: 'OK', onPress: () => this.setState({valid: false})},
+    ])
+
+  }
+  else{
+    this.setState({valid: true})
   }
 }
 
@@ -146,7 +117,7 @@ checkEmpty(){
         
         <TextInput
           style={styles.placeHolderText}
-          placeholder="Date of Birth: yyyy/mm/dd"
+          placeholder="Date of Birth: yyyy-mm-dd"
           onChangeText={(dob) => this.setState({dob})}
           value={this.state.dob}
         />        
@@ -168,17 +139,7 @@ checkEmpty(){
           
         <TouchableHighlight
                         style={styles.button}
-                        onPress={() => {
-                          this.props.navigation.navigate('LocationInfo', {
-                              role: role,
-                              fname: this.state.fname,
-                              lname: this.state.lname,
-                              gender: this.state.gender,
-                              dob: this.state.dob,
-                              addr: this.state.addr,
-                              height: this.state.height,
-                              weight: this.state.weight});
-                      }}
+                        onPress={() => this.CheckPage()}
                     >
                         <Text style={styles.btnText}> LOCATION INFO </Text>
         </TouchableHighlight>     
